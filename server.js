9c94683929574f238a3d11af4a9858173d1da3bb
Mal <malamationsofficial+github@gmail.com>
2025-01-20 03:18:04 -0700

    Update server.js, links were outdated

--- server.js
+++ server.js
@@ -30,35 +30,35 @@ app.get('/download', (req, res) => {
 })
 
 app.get('/download/winlatest', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/wrapper-offline-win32-x64.zip')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-latest/wrapper-offline-win32-x64.zip')
 })
 
 app.get('/download/osxlatest', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/Wrapper.Offline.dmg')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-latest/Wrapper.Offline.dmg')
 })
 
 app.get('/download/tuxlatest', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/wrapper-offline-linux-x64.tar.gz')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-latest/wrapper-offline-linux-x64.tar.gz')
 })
 
 app.get('/download/win2.0.0', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-legacy/2.0.0/wrapper-offline-win32-x64.zip')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-legacy/2.0.0/wrapper-offline-win32-x64.zip')
 })
 
 app.get('/download/osx2.0.0', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-legacy/2.0.0/Wrapper.Offline-macos-x64.zip')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-legacy/2.0.0/Wrapper.Offline-macos-x64.zip')
 })
 
 app.get('/download/tux2.0.0', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-legacy/2.0.0/wrapper-offline-linux-x64.zip')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-legacy/2.0.0/wrapper-offline-linux-x64.zip')
 })
 
 app.get('/download/src-2.0.0-zip', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-src/wrapper-offline-2.0.0.zip')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-src/wrapper-offline-2.0.0.zip')
 })
 
 app.get('/download/src-2.0.0-tarball', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-src/wrapper-offline-2.0.0.tar.gz')
+    res.redirect('https://storage.googleapis.com/wrapper-offline-src/wrapper-offline-2.0.0.tar.gz')
 })
 
 app.listen(3000)
