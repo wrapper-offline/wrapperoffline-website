16bf0346864c8c278dde08ef36baeea63479570e
Mal <malamationsofficial+github@gmail.com>
2025-01-20 02:43:40 +0000

    Minimal version of the site so we can get it out ASAP. Full site on main branch.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/download', (req, res) => {
-    res.render('downloads', { latestVer: "2.0.0" }) // Change this whenever new version is released
+    res.render('downloads', { latestVer: "2.0.1" }) // Change this whenever new version is released
 })
 
 app.get('/download/winlatest', (req, res) => {
@@ -34,11 +34,23 @@ app.get('/download/winlatest', (req, res) => {
 })
 
 app.get('/download/osxlatest', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/Wrapper.Offline-macos-x64.zip')
+    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/Wrapper.Offline.dmg')
 })
 
 app.get('/download/tuxlatest', (req, res) => {
-    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/wrapper-offline-linux-x64.zip')
+    res.redirect('https://storage.cloud.google.com/wrapper-offline-latest/wrapper-offline-linux-x64.tar.gz')
+})
+
+app.get('/download/win2.0.0', (req, res) => {
+    res.redirect('https://storage.cloud.google.com/wrapper-offline-legacy/2.0.0/wrapper-offline-win32-x64.zip')
+})
+
+app.get('/download/osx2.0.0', (req, res) => {
+    res.redirect('https://storage.cloud.google.com/wrapper-offline-legacy/2.0.0/Wrapper.Offline-macos-x64.zip')
+})
+
+app.get('/download/tux2.0.0', (req, res) => {
+    res.redirect('https://storage.cloud.google.com/wrapper-offline-legacy/2.0.0/wrapper-offline-linux-x64.zip')
 })
 
 app.get('/download/src-2.0.0-zip', (req, res) => {
